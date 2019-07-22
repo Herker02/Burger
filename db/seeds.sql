1d659ee677f3d33927ffe873cfecfa8ac74240cb
HerKer02 <herker02@gmail.com>
2019-07-21 23:55:25 -0500

    final

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bleu Cheese Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Jack Daddy Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger", true);
